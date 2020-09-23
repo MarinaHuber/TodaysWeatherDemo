e7d41bdb75119bea22686f5a29aa3d2584207eba
Marina Huber <huber.marina@gmail.com>
2020-09-23 17:18:02 +0200

    Add setUp model without CodingKeys

--- ProjectSwift/ProjectSwift/Models/CityWeather.swift
+++ ProjectSwift/ProjectSwift/Models/CityWeather.swift
@@ -10,5 +10,41 @@ import Foundation
 
 
 struct CityWeather: Codable {
+    let weather: [WeatherSingle]?
+    let base: String?
+    let main: MainSingle?
+    let visibility: Int?
+    let dt: Int?
+    let sys: System?
+    let id: Int?
     let name: String?
+    let cod: Int?
+
+}
+
+struct WeatherSingle: Codable {
+    let id: Int?
+    let main: String?
+    let description: String?
+    let icon: String?
+
+}
+
+struct System: Codable {
+    let type: Int?
+    let id: Int?
+    let message: Double?
+    let country: String?
+    let sunrise: UInt64?
+    let sunset: UInt64?
+}
+
+struct MainSingle: Codable {
+
+    let temp: Double?
+    let pressure: Double?
+    let humidity: Int?
+    let temp_max: Double?
+    let temp_min: Double?
+
 }
